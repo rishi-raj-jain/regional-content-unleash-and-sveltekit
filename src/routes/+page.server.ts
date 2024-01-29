7689c360a98051b34f2c51b15beaf0d314df39f2
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-29 21:47:48 +0530

    log region

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,7 @@
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ request }) => {
-    const region: "US" | "IN" | string = request.headers.get('x-vercel-ip-country') ?? "US"
+    const region: string = request.headers.get('x-vercel-ip-country') ?? "US"
+    console.log(region)
     return { region }
 }
