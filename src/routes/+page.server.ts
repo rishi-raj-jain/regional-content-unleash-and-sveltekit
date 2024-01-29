3572d351578f51818e6b26cf22ce0bac0b8be5c8
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-29 22:03:36 +0530

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,7 @@
 import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ request }) => {
+    // get the vercel IP country header containing 2 letter string for that country
     const region: string = request.headers.get('x-vercel-ip-country') || "US"
     return { region }
 }
