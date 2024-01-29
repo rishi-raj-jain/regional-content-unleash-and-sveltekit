e8a62f6837d6eaddef2ffc1ceefbfa4445e0c64b
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-29 22:04:05 +0530

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+// create a super collection mapping country to the language used in that region
 export const countryCodeMap: { [k: string]: string } = {
     "IN": "HI",
     "US": "EN",
