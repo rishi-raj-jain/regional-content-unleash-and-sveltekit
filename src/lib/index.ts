96079b278c2c211d7368bbe3016fe367828c6e8f
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2024-01-29 22:05:14 +0530

    add components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 // create a super collection mapping country to the language used in that region
-export const countryCodeMap: { [k: string]: string } = {
+export const countryCodeMap: Record<string, string> = {
     "IN": "HI",
     "US": "EN",
 }
